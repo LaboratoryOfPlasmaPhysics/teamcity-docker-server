c5dc0e7022523ab5a077d3d69d3379a4bb927877
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-03-07 11:21:24 +0100

    Fix build scripts
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-docker build -t  129.104.6.165:32219/lpp/teamcity-server .
-docker push 129.104.6.165:32219/lpp/teamcity-server
+docker build -t  129.104.6.172:32219/lpp/teamcity-server .
+docker push 129.104.6.172:32219/lpp/teamcity-server
 cd postgresql && sh ./build_and_push.sh
--- postgresql/build_and_push.sh
+++ postgresql/build_and_push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-docker build -t 129.104.6.165:32219/lpp/postgres_fr:12 .
-docker push 129.104.6.165:32219/lpp/postgres_fr:12
+docker build -t 129.104.6.172:32219/lpp/postgres_fr:12 .
+docker push 129.104.6.172:32219/lpp/postgres_fr:12
